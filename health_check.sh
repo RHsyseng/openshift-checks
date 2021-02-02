76228a6c326c06e8fb585623ef7463c18dbba2f0
Eduardo Minguez Perez <e.minguez@gmail.com>
2021-02-02 11:07:10 +0100

    Split script into modules

--- health_check.sh
+++ health_check.sh
@@ -1,87 +1,44 @@
 #!/usr/bin/env bash
 
-export errors=0
-export restart_threshold=10 #arbitray
+# https://betterdev.blog/minimal-safe-bash-script-template/
 
-function check_terminating() {
-  terminating_pods=$(oc get pods -A | grep -c 'Terminating')
-  if [[ $terminating_pods -ge 1 ]]; then
-    echo "Pods in Terminating state ($terminating_pods):"
-    oc get pods -A | grep 'Terminating'
-    errors=$(($errors+1))
-  fi
-}
+#set -Eeuo pipefail
 
-function check_operators() {
-  bad_operators=$(oc get co --no-headers | egrep -civ 'True.*False.*False')
-  if [[ $bad_operators -ge 1 ]]; then
-    echo "Operators in Bad State ($bad_operators):"
-    oc get co --no-headers | egrep -iv 'True.*False.*False'
-    errors=$(($errors+1))
-  fi
-}
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+IFS=$'\n\t'
 
-function check_machineconfigs() {
-  degrated_mcps=$(oc get mcp -o json | jq '.items[] | { name: .metadata.name, status: .status } | select (.status.degradedMachineCount >= 1) | { name: .name, status: .status.degradedMachineCount}')
-  if [[ ! -z $degrated_mcps  ]]; then
-    echo "MachineConfigProfiles in Degraded State:"
-    echo $degrated_mcps | jq .
-    errors=$(($errors+1))
-  fi
-}
+# shellcheck disable=2164
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 
-function check_nodes() {
-  nodes_not_ready=$(oc get nodes -o json | jq '.items[] | { name: .metadata.name, type: .status.conditions[] } | select ((.type.type == "Ready") and (.type.status == "False"))')
-  if [[ ! -z $nodes_not_ready ]]; then
-    echo "Nodes in NotReady state:"
-    echo $nodes_not_ready | jq
-    errors=$(($errors+1))
-  fi
-  disabled_nodes=$(oc get nodes -o json | jq '.items[] | { name: .metadata.name, status: .metadata.labels."kubevirt.io/schedulable" } | select (.status == "false")')
-  if [[ ! -z $disabled_nodes ]]; then
-    echo "Nodes in Disabled:"
-    echo $disabled_nodes | jq
-    errors=$(($errors+1))
-  fi
+# shellcheck disable=SC1091
+source ./utils
+
+#trap cleanup SIGINT SIGTERM ERR EXIT
+
+parse_params "$@"
+setup_colors
+
+errors=0
+RESTART_THRESHOLD=${RESTART_THRESHOLD:=10} #arbitray
+
+main() {
+
+  for i in oc jq curl; do
+    check_command ${i}
+  done
   
-}
-
-function check_alertmanager() {
-  # https://access.redhat.com/solutions/4250221
-  alert_url=$(oc -n openshift-monitoring get routes/alertmanager-main -o json | jq -r .spec.host)
-  alerts=$(curl -s -k -H "Authorization: Bearer $(oc -n openshift-monitoring sa get-token prometheus-k8s)" https://$alert_url/api/v1/alerts | jq '.data[].labels |  {alert: .alertname, severity: .severity} | select(.severity == "warning")')
-  
-  if [[ ! -z $alerts ]]; then
-    echo "Alerts currently firing:"
-    echo $alerts | jq
-    errors=$(($errors+1))
-  fi
-
-}
-
-function check_restarts() {
-  restarts=$(oc get pods -o json -A| jq -r ".items[] | { name: .metadata.name, project: .metadata.namespace, restarts: .status.containerStatuses[].restartCount } | select(.restarts > $restart_threshold)")
-  if [[ ! -z $restarts ]]; then
-    echo "Pods that have a high restart count (> $restart_threshold):"
-    echo $restarts | jq .
-    errors=$(($errors+1))
+  kubeconfig
+  OCUSER=$(oc_whoami)
+  msg "Running basic health checks as ${GREEN}${OCUSER}${NOCOLOR}"
+  for check in ./checks/*; do
+    # shellcheck disable=SC1090,SC1091
+    source "${check}"
+  done
+  if [ ${errors} -gt 0 ]; then
+    msg "${RED}Total issues found: ${errors}${NOCOLOR}"
+  else
+    msg "${GREEN}No issues found${NOCOLOR}"
   fi
 }
 
-function main() {
-
-  echo "Running basic health checks"
-
-  check_terminating
-  check_operators
-  check_machineconfigs
-  check_nodes
-  check_alertmanager
-  check_restarts
-
-  echo "Total issues found: $errors"
-  
-
-}
-
-main $@
+main "$@"
